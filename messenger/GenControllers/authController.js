000b920f654e3a1a6257521d071b3c25cd155b13
arct297 <siniov.arseniy@gmail.com>
2025-02-22 15:01:11 +0500

    actual address

--- messenger/GenControllers/authController.js
+++ messenger/GenControllers/authController.js
@@ -30,7 +30,7 @@ exports.signUpUser = async (req, res) => {
         const newUser = new User({ login, password: hashedPassword, email, username, staySignedIn, verificationCode });
         await newUser.save();
 
-        const verificationLink = `https://5fb3-2a0d-b201-43-db51-9ce9-d75-c853-7c3c.ngrok-free.app/auth/confirm?email=${email}&code=${verificationCode}`;
+        const verificationLink = `https://013c-2a0d-b201-e0-38e5-a5f1-8ea2-aaab-d053.ngrok-free.app/auth/confirm?email=${email}&code=${verificationCode}`;
         console.log(verificationLink);
         await emailService.sendVerificationEmail(email, verificationLink);
 
